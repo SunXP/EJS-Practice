f3f2546acbac8ab67c3148aa59e7ff49f95dc34b
SunXP <SunXP@users.noreply.github.com>
2017-08-09 08:36:22 -0400

    Express and EJS practice

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/", function(req, res){
+    res.render("home.ejs");
+});
+
+app.get("/fallinlovewith/:thing", function(req, res){
+    var thing = req.params.thing;
+    res.render("love.ejs", {thingVar: thing});
+    
+});
+
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Check this out", author: "Re"},
+        {title: "Poop", author: "Dave"},
+        {title: "Woo", author: "Don"}
+        ];
+    res.render("posts.ejs", {posts: posts});
+    
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started");
+});
